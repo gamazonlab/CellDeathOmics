120a3084fc5847d29fd69c598c2870dfd02c23c0
Abby Rich <62113259+abby-rich@users.noreply.github.com>
2024-01-29 16:56:39 -0600

    Update MultipleHypothesisTestingCorrectionandFormatting.R
    Updated to include multiple hypothesis testing correction for BioVU replication analysis and XIAP analysis.

--- src/MultipleHypothesisTestingCorrectionandFormatting.R
+++ src/MultipleHypothesisTestingCorrectionandFormatting.R
@@ -1,51 +1,80 @@
-### Multiple Hypothesis Testing Correction (FDR) of all results; Formatting phenotype descriptions
+### Multiple Testing Correction (FDR)
 #   Performed separately for both ICD10/Finngen traits and continuous lab traits
- 
+
+### UKBB PheWAS Multiple Hypothesis Testing Correction -------
+
 # Load packages 
 library(tidyverse)
 library(data.table)
- 
+
 # Load all results where p<0.05 across health traits
-df <- read_csv('data/UKBBv3_AllTraits_FINAL_AllAssociations.csv.gz')
- 
+df <- read_csv('results/UKBBv3_SPrediXcanResults_PCDGenes_FINAL.csv')
+
 # Load cell death gene list of interest dataframe
 pws <- read_csv('metadata/ProgrammedCellDeathGeneList_Pathways.csv')
 genes <- pws$`Gene Name` 
- 
+
 # Load phenotypes of interest dataframe
 pheno <- read_csv('metadata/UKBBv3_AllTraits2Category_FINAL_v2_plusCaseControl.csv')
- 
-# Subset dataframe for genes and phenotypes of interest
-df <- filter(df, gene_name %in% genes, `Phenotype Code` %in% phenos)
-df <- merge(df, pheno, by=c("Phenotype Code", "Derivation"), all.x=T)
-df <- merge(df, pws, by.x="gene_name", by.y="Gene Name", all.x=T)
-df <- select(df, gene_name, Pathway, SubPathway, `Phenotype Description`, Category, zscore, 
-             effect_size, pvalue, Tissue, `Phenotype Code`, gene, 
-             `Protein Name`,var_g, pred_perf_r2, pred_perf_pval, pred_perf_qval, 
-             n_snps_used, n_snps_in_cov, n_snps_in_model, Derivation, File)
-df$Tissue <- str_remove(df$Tissue, "JTI_") %>% str_replace_all("_", " ")
- 
- 
+pheno_ehr <- filter(pheno, Derivation == "ICD10/FinnGen")
+pheno_cont <- filter(pheno, Derivation == "Lab/Continuous")
+
 # FDR Correction, separately on each dataset -------
 # Separate the formatted data
 df_ehr_f <- filter(df, Derivation == "ICD10/FinnGen")
 df_cont_f <- filter(df, Derivation == "Lab/Continuous")
- 
+
 # Determine count of number of tests performed
-JTItissues_genes <- read_csv('metadata/GenesTestedForEachJTITissue.csv')
-JTItissues_cdgenes <- filter(JTItissues_genes, gene_name %in% genes)
+JTItissues_cdgenes <- read_csv('metadata/GenesTestedForEachJTITissue_PCDonly.csv')
 JTItissues_cdgenects <- count(JTItissues_cdgenes, gene_name) %>% arrange(desc(n)) # 5 genes not tested: ACSL4, NOX1/2/3, XIAP
 ntests_ehr <- (sum(JTItissues_cdgenects$n))*(nrow(pheno_ehr))
 ntests_cont <- (sum(JTItissues_cdgenects$n))*(nrow(pheno_cont))
- 
+
 # FDR calculations for all genes and phenotypes of interest
 df_ehr_f$FDR_all <- p.adjust(df_ehr_f$pvalue, method="BH", n=ntests_ehr)
 df_ehr_f <- df_ehr_f %>% arrange(FDR_all) # arrange by FDR
 df_cont_f$FDR_all <- p.adjust(df_cont_f$pvalue, method="BH", n=ntests_cont)
 df_cont_f <- df_cont_f %>% arrange(FDR_all) # arrange by FDR
- 
+
 # Harmonize and format dataframe for saving
 df_fdr <- rbind(df_ehr_f, df_cont_f)
-df_fdr <- df_fdr[,c(1:8,22,9:21)]
- 
-write_csv(df_fdr, "data/UKBBv3_SPrediXcanResults_PCDGenes_FINAL.csv")
+
+write_csv(df_fdr, "results/UKBBv3_SPrediXcanResults_PCDGenes_FINAL.csv")
+# Note: FDR calculations were made in the same fashion for the ferroptosis/cuproptosis/parthanatos analysis
+
+
+### BioVU Validation Multiple Hypothesis Testing Correction -------
+
+# Load data
+df <- read_csv('results/BioVU_SPrediXcanResults_PCDGenes.csv')
+
+# Annotate with corresponding trait name
+pheno <- read_csv('metadata/Dennis2021_LabGWAS_TraitList_v2_Annotated.csv')
+pheno <- select(pheno, Phecode, `Phenotype Description`) %>% drop_na()
+df <- merge(df, pheno, by.x="trait", by.y="Phecode")
+
+# Determine count of number of tests performed
+ntests_validation <- nrow(df)
+
+# FDR calculations for all genes and phenotypes of interest
+df$FDR_all <- p.adjust(df$pvalue, method="BH", n=ntests_validation)
+df <- df %>% arrange(FDR_all) # arrange by FDR
+
+# Harmonize and format dataframe for saving
+df$tissue <- str_replace_all(df$tissue, "_", " ")
+
+# Save dataframe
+write_csv(df, 'results/BioVU_SPrediXcanResults_PCDGenes.csv')
+
+
+### XIAP Results Multiple Hypothesis Testing Correction (Bonferroni) -------
+
+# Load data
+df <- read_csv('results/UKBB_SPrediXcanResults_XIAP.csv')
+
+# Determine count of number of tests performed
+ntests_ehr = 2014 # 2014 tests (19 tissues * 106 phenotypes)
+bonferroni = 0.05/1629
+
+# Subset for results that meet significance
+dfsig <- filter(df, pvalue < bonferroni)
